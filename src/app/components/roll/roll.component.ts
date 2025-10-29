9718c17b4aa0a6789ac78fac42f7733c111a5598
Tim Sands <tdsands@wisc.edu>
2025-10-29 15:37:23 -0500

    added components

---
+++ src/app/components/roll/roll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roll',
+  standalone: true,
+  imports: [],
+  templateUrl: './roll.component.html',
+  styleUrl: './roll.component.scss'
+})
+export class RollComponent {
+
+}
